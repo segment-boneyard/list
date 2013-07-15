72d532d3f188ff2da358dba28aaff4c25f250f28
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-07-14 18:04:41 -0700

    added non-working start of tests

--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,22 @@
-describe('list', function () {});
+describe('List', function () {
+  var assert = require('assert')
+    , List = require('list')
+    , type = require('type');
+
+describe('#el', function () {
+  it('should have an .el property', function () {
+    var list = new List();
+    assert('element' === type(list.el));
+  });
+});
+
+describe('#add(model)', function () {
+  it('should add an item to the list', function () {
+    var list = new List();
+    assert(0 === list.el.childNodes.length);
+    list.add('item');
+    assert(1 === list.el.childNodes.length);
+  });
+});
+
+});
